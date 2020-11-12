2eca4eb7c0f2ae7cd6a20dd71b7fbe20ff05a101
thenist <p717394@gmail.com>
2020-11-12 21:45:13 +0900

    Change tap version to 3.2.5 for 1.16.4 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     compileOnly("com.destroystokyo.paper:paper-api:1.16.4-R0.1-SNAPSHOT")
     compileOnly("org.spigotmc:spigot:1.16.4-R0.1-SNAPSHOT")
 
-    implementation("com.github.noonmaru:tap:3.2.0")
+    implementation("com.github.noonmaru:tap:3.2.5")
     implementation("com.github.noonmaru:kommand:0.6.3")
 
     testImplementation("junit:junit:4.13")
